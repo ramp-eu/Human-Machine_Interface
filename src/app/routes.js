c73411eb0999829ddcc630ca24363d31af65048c
Janne Pajukanta <janne.pajukanta@vtt.fi>
2019-08-13 13:37:42 +0300

    Removed floorplan bmp and tiff filetypes

--- src/app/routes.js
+++ src/app/routes.js
@@ -10,7 +10,8 @@ var pjson = require('../package.json');
 var MAGIC_NUMBERS = {
     jpg: 'ffd8ffe0',
     jpg1: 'ffd8ffe1',
-    png: '89504e47'
+    png: '89504e47',
+    gif: '47494638'
 }
 function checkMagicNumbers(magic) {
     if (magic == MAGIC_NUMBERS.jpg || magic == MAGIC_NUMBERS.jpg1 || magic == MAGIC_NUMBERS.png || magic == MAGIC_NUMBERS.gif) return true;
